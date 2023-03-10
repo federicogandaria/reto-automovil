9f18ff4426ceabe31c490ce7a24d0e9a97292802
matisouza <matias.souza@sofka.com.co>
2023-03-10 16:45:40 -0300

    inicio de proyecto
    se creó el modulo client y mas nada

---
+++ src/cliente/cliente.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ClienteController } from './controller/cliente.controller';
+import { ClienteService } from './service/cliente.service';
+
+@Module({
+  controllers: [ClienteController],
+  providers: [ClienteService]
+})
+export class ClienteModule {}
---
+++ src/cliente/entities/cliente.entity.ts
@@ -0,0 +1,4 @@
+import { uuid } from 'uuidv4';
+
+export class Cliente {
+}
